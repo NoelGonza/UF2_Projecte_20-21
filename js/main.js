05f3343cdc978ef3f11c51839966a56b39cab85a
NoelGonza <noel.gonzalez.santos@gmail.com>
2020-12-12 12:58:26 +0100

    ngs - mostra_numeros 80%

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ window.onload = function(){
     document.getElementById("estats").style.display = "none";
     document.getElementById("tauler").style.display = "none";
 }
+
+document.getElementById("mostra").addEventListener("click", mostra_numeros);
+
+function mostra_numeros(){
+    num1 = document.getElementById("t1").value;
+    num2 = document.getElementById("t2").value;
+
+    document.write("<p>"+ num1 +"</p>");
+    document.write("<p>"+ num2 +"</p>");
+}
