7db0ff12494e8814785210d8cd133a6187fd5cb8
MarcOrtizBurgos <marcortizburgos@gmail.com>
2020-12-12 12:33:23 +0100

    mob - principi objectiu1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 window.onload = function(){
-    prompt("Mida del tauler? 5<->20")
-    partida.iniciar(mida);
+    /* prompt("Mida del tauler? 5<->20")
+    partida.iniciar(mida); */
+    document.getElementById("estats").style.display = "none";
+    document.getElementById("tauler").style.display = "none";
 }
