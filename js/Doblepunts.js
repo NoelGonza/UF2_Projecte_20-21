41d0c6e2d9ca32258b94161b99eac2a38ec4d609
NoelGonza <noel.gonzalez.santos@gmail.com>
2020-12-20 15:31:58 +0100

    ngs - estrellas puestas

--- js/Doblepunts.js
+++ js/Doblepunts.js
@@ -1,5 +1,10 @@
 var Doblepunts = function(){
     Recompensa.apply(this, arguments);
+
+    this.Doblar = function(){
+        Partida.punts = Partida.punts * 2;
+        document.getElementById("punt").innerHTML = Partida.punts;
+    }
 };
 
 Doblepunts.prototype = Object.create(Recompensa.prototype);
--- js/partida.js
+++ js/partida.js
@@ -6,14 +6,13 @@ let Partida = {
     estrelles: [],
     gespa: [],
     vidas: 3,
-    punts: 100,
-    ancho: 0,
-    largo: 0,
+    punts: 0,
+    victoria: 0,
 
     inicialitzar_tauler: function(ample,llarg){
         for (let i=0;i < ample; i++){
             this.tauler[i]=[];
-            for(let j=0;j < llarg; j++){
+            for (let j=0;j < llarg; j++){
                 //Definimos gespa i la guardamos en la array gespa i la añadimos en la array tauler tambien
                 var gespa1 = new Gespa(null, [i,j],"g",50);
                 this.gespa.push(gespa1);
@@ -30,7 +29,7 @@ let Partida = {
         for (let i=0;i < num1; i++){
             aux1 = abc[i];
             tablero += "<div id='"+aux1+"' class='pepe'>";
-            for(let j=0;j < num2; j++){
+            for (let j=0;j < num2; j++){
                 aux2 = abc[j];
                 tablero += "<div id='"+aux1+aux2+"' class='pope'>"+this.tauler[i][j]+"</div>";
             }
@@ -48,69 +47,86 @@ let Partida = {
     insertar_valores: function(){
         let valor = Math.floor(this.max_tauler / 4)
         console.log(valor)
-        for(let i=0;i<this.tauler.length;i++){
-            for(let j=0;j<this.tauler.length;j++){
+        for (let i=0;i<this.tauler.length;i++){
+            for (let j=0;j<this.tauler.length;j++){
                 console.log(this.tauler[i][j] == this.zombis[1])
             }
         }
     },
 
-    crear_objetos: function(){
-        
+    crear_objetos: function(mida1, mida2){
+        porcentaje = Math.round((mida1 * mida2)/4);
+        estrellas = Math.round((parseInt(mida1)+parseInt(mida2))/2);
+        this.crear_zombies(mida1,mida2,porcentaje);
+        this.crear_estrellas(mida1,mida2,estrellas);
     },
 
-    random_matriz: function(PosX,PosY){
-        for(let i=0;i < 3;i++){
-            PosX = Math.floor(Math.random()*(this.tauler.length));
-            PosY = Math.floor(Math.random()*(this.tauler.length));
-            console.log(PosX);
-            console.log(PosY);
-            if(this.tauler[PosX][PosY] == this.zombis[i]){
-                console.log("No puedes")
+    crear_zombies: function(PosX,PosY,Por25){
+        for (let i=0;i < Por25;i++){
+            do{
+                PosX = Math.floor(Math.random()*(this.tauler.length));
+                PosY = Math.floor(Math.random()*(this.tauler[0].length));
+                console.log(PosX);
+                console.log(PosY);
             }
-            else{
-                //definimos valores de zombie
-                var pivote = new Zombi(null, [PosX,PosY],"z",100);
-                this.zombis.push(pivote);
-                var cambia = pivote.ModificaArray();
-                this.tauler[PosX][PosY] = cambia;
-                console.log(this.zombis);
-                console.log(this.tauler);
-            }
+            while (this.tauler[PosX][PosY] != "g");
+            
+            var pivote = new Zombi(null, [PosX,PosY],"z",100);
+            this.zombis.push(pivote);
+            var cambia = pivote.ModificaArray();
+            this.tauler[PosX][PosY] = cambia;
         }
-        console.log(this.zombis)
+        console.log(this.zombis);
+        console.log(this.tauler);
+    },
+
+    crear_estrellas: function(PosX,PosY,Por25){
+        for (let i=0;i < Por25;i++){
+            do{
+                PosX = Math.floor(Math.random()*(this.tauler.length));
+                PosY = Math.floor(Math.random()*(this.tauler[0].length));
+                console.log(PosX);
+                console.log(PosY);
+            }
+            while (this.tauler[PosX][PosY] != "g");
+            
+            var pivote = new Estrella(null, [PosX,PosY],"e",200);
+            this.estrelles.push(pivote);
+            var cambia = pivote.ModificaArray();
+            this.tauler[PosX][PosY] = cambia;
+        }
+        console.log(this.estrelles);
+        console.log(this.tauler);
     },
 
     iniciar: function(mida1,mida2){
         //Ponemos los valores de vida i puntos
         document.getElementById("vid").innerHTML = this.vidas;
         document.getElementById("punt").innerHTML = this.punts;
-        ancho = mida1;
-        largo = mida2;
         this.max_tauler = mida1 * mida2;
         this.inicialitzar_tauler(mida1,mida2);
         this.mostrar_tauler(mida1,mida2);
-        this.random_matriz(mida1,mida2);
+        this.crear_objetos(mida1,mida2);
         console.log(this.tauler);
         this.insertar_valores();
     }
 }
 
-document.getElementById("busca").addEventListener("click", busca_zombie);
+document.getElementById("busca").addEventListener("click", buscar_obj);
 
-function busca_zombie(){
+function buscar_obj(){
     let posX = document.getElementById("z1").value;
     let posY = document.getElementById("z2").value;
     if (Partida.tauler[posX][posY] == "z"){
         for (let i=0; i < Partida.zombis.length; i++) {
             //Restamos una vida cada vez que se ejecuta este if porque emos encontrado un zombie
-            if(Partida.zombis[i].pos1[0] == posX && Partida.zombis[i].pos1[1] == posY){
+            if (Partida.zombis[i].pos1[0] == posX && Partida.zombis[i].pos1[1] == posY){
                 Partida.tauler[posX][posY] = Partida.zombis[i].Descobert();
                 Partida.mostrar_tauler(Partida.tauler.length,Partida.tauler[0].length);
                 Partida.vidas--;
                 document.getElementById("vid").innerHTML = Partida.vidas;
                 //Si los puntos son mas grandes o igual a 100 le restamos los 100 que estan definidos en zombie
-                if(Partida.punts >= 100){
+                if (Partida.punts >= 100){
                     Partida.punts = Partida.punts - Partida.zombis[i].Puntuaciones();
                     document.getElementById("punt").innerHTML = Partida.punts;
                 }
@@ -120,7 +136,7 @@ function busca_zombie(){
                     document.getElementById("punt").innerHTML = Partida.punts;
                 }
                 //Si las vidas llegan a 0 pierdes
-                if(Partida.vidas == 0){
+                if (Partida.vidas == 0){
                     alert("Has perdido wey :(");
                 }
             }
@@ -129,7 +145,7 @@ function busca_zombie(){
     //Else if de gespa que si la encuentra la canvia a mayus i suma 50 puntos a puntuacion
     else if (Partida.tauler[posX][posY] == "g"){
         for (let i=0; i < Partida.gespa.length; i++){
-            if(Partida.gespa[i].pos1[0] == posX && Partida.gespa[i].pos1[1] == posY){
+            if (Partida.gespa[i].pos1[0] == posX && Partida.gespa[i].pos1[1] == posY){
                 Partida.tauler[posX][posY] = Partida.gespa[i].Descobert();
                 Partida.mostrar_tauler(Partida.tauler.length,Partida.tauler[0].length);
                 Partida.punts = Partida.punts + Partida.gespa[i].Puntuaciones();
@@ -139,13 +155,18 @@ function busca_zombie(){
     }
     //Else if para mas adelante buscar si es una estrella al crearla hay que retocar
     else if (Partida.tauler[posX][posY] == "e"){
+        let ganar = Partida.estrelles.length;
         for (let i=0; i < Partida.estrelles.length; i++){
-            if(Partida.estrelles[i].pos1[0] == posX && Partida.estrelles[i].pos1[1] == posY){
+            if (Partida.estrelles[i].pos1[0] == posX && Partida.estrelles[i].pos1[1] == posY){
                 Partida.tauler[posX][posY] = Partida.estrelles[i].Descobert();
+                Partida.victoria++;
                 Partida.mostrar_tauler(Partida.tauler.length,Partida.tauler[0].length);
                 Partida.punts = Partida.punts + Partida.estrelles[i].Puntuaciones();
                 document.getElementById("punt").innerHTML = Partida.punts;
             }
+        }
+        if (Partida.victoria == ganar){
+            alert("Has ganado mi niño");
         }
     }
 }
